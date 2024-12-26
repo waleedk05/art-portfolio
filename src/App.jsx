1832294470ef4115961bc756d78519a4a6d7c808
Waleed Ahmed <wahmed02@teresol.com>
2024-12-26 17:58:18 +0500

    v1.0.2

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
-import "./App.css";
 import Gallery from "./components/Gallery";
 import About from "./components/About";
 import Contact from "./components/Contact";
